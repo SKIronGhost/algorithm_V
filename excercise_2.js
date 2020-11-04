e23da0b53f86a9aca501fdac0b585e9a6e2fbb1f
SKIronGhost <ed.diegofelix@gmail.com>
2020-11-03 23:03:55 -0300

    update excercise 4

--- excercise_2.js
+++ excercise_2.js
@@ -1,9 +1,12 @@
 // Dado un array, mueve todos los valores un espacio de derecha a izquierda eliminando el primer valor y dejando un 0 para el último valor. Por ejemplo, moverAdelante([1,2,3]) debiera dar como resultado [2,3,0].
 
 function moverAdelante(x){
-  x.splice(0,1);
-  x.push(0);
-  return x;
+  var z=[]
+  for (i=1;i<x.length;i++){
+    z.push(x[i])
+  }
+  z.push(0)
+  return z;
 }
 
-console.log(moverAdelante([1,2,3]))
+console.log(moverAdelante([1,2,3]));
